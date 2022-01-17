ae98f08d2e1ff4e6f895d402efdb1fc9ff14cc51
thealexcesar <thealexcesar@gmail.com>
2022-01-17 14:44:49 -0300

    I18n updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :set_user_id, only: :enable_disable
+
   def index
     @users = User.all
   end
