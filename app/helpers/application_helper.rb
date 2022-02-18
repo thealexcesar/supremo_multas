2a581c2a97673cb039e275b1490ccd53662acbda
thealexcesar <thealexcesar@gmail.com>
2022-02-18 19:11:30 -0300

    stylesheets and main front pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,10 @@ module ApplicationHelper
   end
   def new_model
     model_name = controller_name.classify.underscore
+    t("general.new_model", model: t("activerecord.models.#{model_name}") )
+  end
+  def new_model_f
+    model_name = controller_name.classify.underscore
     t("general.new_model_f", model: t("activerecord.models.#{model_name}") )
   end
 
